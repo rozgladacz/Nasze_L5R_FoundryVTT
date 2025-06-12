5edca51f8d86c13c9629a350f368bfe1ebc6e10a
Litasa <jakob.ruhl@gmail.com>
2025-06-12 08:33:27 +0200

    Adding system badge to settings menu. Idea stolen from DnD

--- system/scripts/hooks.js
+++ system/scripts/hooks.js
@@ -116,17 +116,53 @@ export default class HooksL5r5e {
                 // Add title on button dice icon
                 html.find(".chat-control-icon")[0].title = game.i18n.localize("l5r5e.dice.dicepicker.title");
                 break;
+            }
+    }
 
-            case "settings":
-                // Add Changelog link
-                html.find("#game-details .system").append(
-                    `<span><a href="${game.system.changelog}" target="_blank">Changelog</a>`
-                    + ` <a href="${game.i18n.localize("l5r5e.settings.wiki.link")}" target="_blank">${game.i18n.localize("l5r5e.settings.wiki.title")}</a>`
-                    + ` <a href="${game.i18n.localize("l5r5e.settings.custom-compendiums.link")}" target="_blank">${game.i18n.localize("l5r5e.settings.custom-compendiums.title")}</a>`
-                    + `</span>`
-                );
-                break;
-        }
+    static activateSettings(app) {
+        const html = app.element
+        const pip = html.querySelector(".info .system .notification-pip");
+        html.querySelector(".info .system").remove();
+      
+        const section = document.createElement("section");
+        section.classList.add("l5r5e", "sidebar-info");
+        section.innerHTML = `
+          <h4 class="divider">${game.i18n.localize("WORLD.FIELDS.system.label")}</h4>
+          <div class="l5r5e system-badge">
+            <img src="systems/l5r5e/assets/l5r-logo.webp" data-tooltip="${game.i18n.localize("l5r5e.title")}" alt="${game.i18n.localize("l5r5e.title")}">
+            <span class="system-info">${game.system.version}</span>
+          </div>
+        `;
+
+        const links = document.createElement("ul");
+        links.classList.add("unlist", "links");
+        links.innerHTML = `
+        <li>
+            <a href="${game.i18n.localize("l5r5e.settings.notes.link")}" target="_blank">
+            ${game.i18n.localize("l5r5e.settings.notes.title")}
+            </a>
+        </li>
+        <li>
+            <a href="${game.i18n.localize("l5r5e.settings.issues.link")}" target="_blank">${game.i18n.localize("l5r5e.settings.issues.title")}</a>
+        </li>
+        <li>
+            <a href="${game.i18n.localize("l5r5e.settings.wiki.link")}" target="_blank">${game.i18n.localize("l5r5e.settings.wiki.title")}</a>
+        </li>
+        <li>
+            <a href="${game.i18n.localize("l5r5e.settings.custom-compendiums.link")}" target="_blank">
+            ${game.i18n.localize("l5r5e.settings.custom-compendiums.title")}
+            </a>
+        </li>
+        <li>
+            <a href="${game.i18n.localize("l5r5e.logo.discord.link")}" target="_blank">
+            ${game.i18n.localize("l5r5e.logo.discord.title")}
+            </a>
+        </li>
+        `;
+
+        section.append(links);
+        if ( pip ) section.querySelector(".system-info").insertAdjacentElement("beforeend", pip);
+        html.querySelector(".info").insertAdjacentElement("afterend", section);
     }
 
     /**
