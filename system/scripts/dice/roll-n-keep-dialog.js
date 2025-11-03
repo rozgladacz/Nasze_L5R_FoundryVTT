43d2e5613dd539f3a8b3f19801862987f1037636
rozgladacz <rozgladacz@gmail.com>
2025-11-03 13:34:45 +0100

    Restrict roll n keep parameter editing for non-owners

--- system/scripts/dice/roll-n-keep-dialog.js
+++ system/scripts/dice/roll-n-keep-dialog.js
@@ -2197,6 +2197,17 @@ export class RollnKeepDialog extends FormApplication {
                 if (isLocked) {
                     decorated.isEditable = false;
                 }
+
+                if (parameter) {
+                    parameter.editable = Boolean(parameter.editable) && isEditable;
+                    parameter.isEditable = Boolean(parameter.isEditable) && isEditable;
+                }
+
+                if (decorated) {
+                    decorated.editable = Boolean(decorated.editable) && isEditable;
+                    decorated.isEditable = Boolean(decorated.isEditable) && isEditable;
+                }
+
                 return decorated;
             });
 
